dd8c9828f0301dda6f202ff79a279a6ce1411957
vinay techdev <vinayvinuu629@gmail.com>
2025-07-25 11:03:17 +0530

    fixing for vercel deploy

--- Server/server.js
+++ Server/server.js
@@ -57,5 +57,10 @@ app.use('/api/messages', msgRouter);
 // connected to mongoDB
 await connectDB()
 
+if(process.env.NODE_ENV !== "production"){
 const PORT = process.env.PORT || 5000;
 server.listen(PORT, () => console.log(`Server is running on PORT: ${PORT}`));
+}
+
+// export server for vercel
+export default server;
